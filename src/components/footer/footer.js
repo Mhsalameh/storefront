9b40993503099c8135810336acf6a8b16eec5414
Mhsalameh <mohsalameh1@gmail.com>
2022-05-30 22:25:54 +0300

    uml

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material';
 export default function Footer() {
   return (
     <Typography id='footer' align='justify' variant='subtitle'>
-      &copy; 2020 Not A Real Store : Mohammad Salameh
+      &copy; 2022 Not A Real Store : Mohammad Salameh
     </Typography>
   );
 }
