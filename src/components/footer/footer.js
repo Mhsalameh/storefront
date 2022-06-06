2694048de9371c414ca2498275d8122c2ba12e6d
Mhsalameh <mohsalameh1@gmail.com>
2022-06-06 19:44:22 +0300

    a bit of tweaking, created routes and details pages

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import { Typography } from '@mui/material';
 
 export default function Footer() {
   return (
-    <Typography id='footer' align='justify' variant='subtitle'>
+    <Typography marginTop='2rem' id='footer' align='justify' variant='subtitle'>
       &copy; 2022 Not A Real Store : Mohammad Salameh
     </Typography>
   );
--- src/components/products/details.js
+++ src/components/products/details.js
@@ -1,78 +1,93 @@
-import { Modal } from '@mui/material';
 import { connect } from 'react-redux';
 import { Typography } from '@mui/material';
-import { Button } from '@mui/material';
-import { useState } from 'react';
-import Card from '@mui/material/Card';
+import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { addToCart } from '../../store/cart';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useParams } from 'react-router-dom';
+import { getAllProducts, updateProduct } from '../../store/actions';
+import SimpleCart from '../cart/simplecart';
 function Details(props) {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const { products, getAllProducts, addToCart, updateProduct, cart } = props;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '30%',
-    height: 'auto',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-  const { item } = props;
+  useEffect(() => {
+    getAllProducts();
+  }, [getAllProducts]);
+
+  useEffect(() => {
+    setProduct(products.find((product) => product.id === parseInt(id)));
+  }, [products, id]);
+
+  //render a product info page with the product info and a button to add to cart if the product is in stock
+  //also show related products
+  //the header of the page should be the product name
   return (
-    <div>
-      <Button onClick={handleOpen}>view Details</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
+    <>
+      {cart.items.length ? <SimpleCart /> : null}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+          maxHeight: '600px',
+          minHeight: '700px',
+        }}
       >
-        <Box>
-          <Card sx={style}>
-            <CardMedia
-              sx={{
-                maxWidth: '100%',
-                height: '300px',
-              }}
-              image={item.image}
-              title={item.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant='h2' component='h2'>
-                {item.name}
-              </Typography>
-              <Typography variant='h5' color='textSecondary' component='p'>
-                {item.description}
-              </Typography>
-              <Typography variant='h5' color='textSecondary' component='p'>
-                price: ${item.price}
-              </Typography>
-              <br />
-              <Button
-                variant='contained'
-                color='primary'
-                onClick={() => {
-                  handleClose();
-                }}
-              >
-                close
-              </Button>
-            </CardContent>
-          </Card>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row-reverse',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <div style={{}}>
+            <Typography variant='h5'>{product?.name}</Typography>
+            <Typography variant='body1'>{product?.description}</Typography>
+            <Typography variant='body2'>
+              {product?.inventory
+                ? `${product.inventory} items In Stock`
+                : 'Out of Stock'}
+            </Typography>
+            <Typography variant='body1'>${product?.price}</Typography>
+          </div>
+          <img
+            src={product?.image}
+            alt={product?.name}
+            style={{
+              width: '29%',
+              height: '29%',
+              objectFit: 'cover',
+            }}
+          />
         </Box>
-      </Modal>
-    </div>
+        <Button
+          variant='contained'
+          onClick={() => {
+            addToCart(product);
+            updateProduct('decrement', product);
+          }}
+          sx={{
+            marginTop: '1rem',
+          }}
+        >
+          <AddShoppingCartIcon />
+          Add to Cart
+        </Button>
+      </Box>
+      <br />
+    </>
   );
 }
 
 const mapStateToProps = (state) => ({
   products: state.products.products,
+  cart: state.cart,
 });
-export default connect(mapStateToProps)(Details);
+const mapDispatchToProps = { getAllProducts, updateProduct, addToCart };
+export default connect(mapStateToProps, mapDispatchToProps)(Details);
